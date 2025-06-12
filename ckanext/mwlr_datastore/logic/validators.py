7561d83f7df2905d81af7a541a940e294925d162
Chris Wydra <wydrac@landcareresearch.co.nz>
2025-06-12 16:01:10 +1200

    Refactor Dockerfile and Docker Compose for improved dependency management and context setup; add validators for enhanced data validation in MWLR Datastore plugin.

---
+++ ckanext/mwlr_datastore/logic/validators.py
@@ -0,0 +1,194 @@
+import ckan.plugins.toolkit as tk
+from ckan.plugins.toolkit import Invalid
+from ckan.lib.navl.dictization_functions import Missing
+import sys
+import datetime
+import math
+import logging
+
+def is_missing_or_empty(data,key):
+    """Convenience function to see if an element of a data dict is
+    not there, Missing, None, or the empty string."""
+    key = (key,)
+    return (key not in data
+            or isinstance(data[key],Missing)
+            or data[key] is None
+            or data[key] == '')
+
+def is_number(value):
+    '''Validates value is a number'''
+    if value is None:
+        return None
+    if hasattr(value, 'strip') and not value.strip():
+        return None
+
+    try :
+        float(value)
+    except ValueError :
+        raise Invalid("The value is not a number")
+
+    return value
+
+def is_elevation_range(value):
+    '''Validates value is within a valid range'''
+    if value is None:
+        return None
+    if hasattr(value, 'strip') and not value.strip():
+        return None
+
+    try :
+        r = float(value)
+        if -20 < r and r < 3000 :
+            return value
+        else :
+            raise Invalid("The value is not a number between -20 and 3000")
+    except ValueError :
+        raise Invalid("The value is not a number between -20 and 3000")
+
+    return value
+
+def is_year(value):
+    '''Validates value is the date format YYYY'''
+    try:
+        datetime.datetime.strptime(value, '%Y')
+    except ValueError:
+        raise Invalid("Year must be in the format YYYY, e.g. 2015")
+    return value
+
+
+def is_year_month(value):
+    '''Validates value is in the date format YYYY-MM'''
+    try:
+        datetime.datetime.strptime(value, '%Y-%m')
+    except ValueError:
+        raise Invalid("Date must be in the format YYYY-MM, e.g. 2015-01")
+    return value
+
+
+def is_year_month_day(value):
+    '''Validates value is in the date format YYYY-MM-DD'''
+    try:
+        datetime.datetime.strptime(value, '%Y-%m-%d')
+    except ValueError:
+        raise Invalid("Date must be in the format YYYY-MM-DD, e.g. 2015-01-29")
+    return value
+
+
+def is_date(value):
+    '''Validates value is in one of three various formats'''
+    for m in ['is_year', 'is_year_month', 'is_year_month_day']:
+        try:
+            getattr(sys.modules[__name__], m)(value)
+        except Invalid:
+            pass
+        else:
+            return value
+
+    raise Invalid("Date must be in the format YYYY-MM-DD, YYYY-MM, or YYYY")
+
+## === for creating a bounding box around a lat/lon === #
+## Semi-axes of WGS-84 geoidal reference
+WGS84_a = 6378137.0  # Major semiaxis [m]
+WGS84_b = 6356752.3  # Minor semiaxis [m]
+
+## Earth radius at a given latitude, according to the WGS-84 ellipsoid [m]
+def WGS84EarthRadius(lat):
+    ## http://en.wikipedia.org/wiki/Earth_radius
+    An = WGS84_a*WGS84_a * math.cos(lat)
+    Bn = WGS84_b*WGS84_b * math.sin(lat)
+    Ad = WGS84_a * math.cos(lat)
+    Bd = WGS84_b * math.sin(lat)
+    return math.sqrt( (An*An + Bn*Bn)/(Ad*Ad + Bd*Bd) )
+
+## Degrees to radians
+def deg2rad(degrees):
+    return math.pi*degrees/180.0
+
+## Radians to degrees
+def rad2deg(radians):
+    return 180.0*radians/math.pi
+
+## Bounding box surrounding the point at given coordinates,
+## assuming local approximation of Earth surface as a sphere
+## of radius given by WGS84
+def boundingBox(lat, lon, halfSideHeightInKm, halfSideWidthInKm):
+    ## lat, lon = tuple(map(deg2rad, (lat, lon)))
+    lat = deg2rad(lat)
+    lon = deg2rad(lon)
+    halfSideH = 1000*halfSideHeightInKm
+    halfSideW = 1000*halfSideWidthInKm
+    ## Radius of Earth at given latitude
+    radius = WGS84EarthRadius(lat)
+    ## Radius of the parallel at given latitude
+    pradius = radius*math.cos(lat)
+    latMin = lat - halfSideH/radius
+    latMax = lat + halfSideH/radius
+    lonMin = lon - halfSideW/pradius
+    lonMax = lon + halfSideW/pradius
+    return tuple(map(rad2deg, (latMin, lonMin, latMax, lonMax)))
+
+def convert_spatial(key, data, errors, context):
+    """Computes a spatial extent polygon from latitude / longitude if
+    necessary. See scheming/dataset.yml for the definition of data
+    fields."""
+    #logger = logging.getLogger(__name__)
+    #logger.info("[convert_spatial-data] = %s" % data)
+    if all([is_missing_or_empty(data,key) for key in ('latitude','longitude','area_height','area_width')]):
+        ## no data to set bbox - do nothing
+        pass
+    elif not is_missing_or_empty(data,'spatial'):
+        ## user has set the bbox-  do nothing
+        pass
+    else:
+        if is_missing_or_empty(data,'area_height'):
+            ## set default height
+            data[('area_height',)] = 0.05 # 50m
+        if is_missing_or_empty(data,'area_width'):
+            ## set default width
+            data[('area_width',)] = 0.05 # 50m
+        if any([is_missing_or_empty(data,key) for key in ('latitude','longitude','area_height','area_width')]):
+            ## incomplete set of data to define bbox
+            raise Invalid("Incomplete set of elements defining spatial bbox: latitude, longitude, area_height (has default), area_width (has default)")
+        else:
+            ## compute spatial extent
+            bbox = boundingBox(
+                float(data[('latitude',)]),
+                float(data[('longitude',)]),
+                float(data[('area_height',)])/2.0,
+                float(data[('area_width',)])/2.0,)
+            ## store coordinates as lon / lat
+            upper_left  = (str(bbox[3]),str(bbox[2]))
+            upper_right = (str(bbox[3]),str(bbox[0]))
+            lower_right = (str(bbox[1]),str(bbox[0]))
+            lower_left  = (str(bbox[1]),str(bbox[2]))
+            polygon = "{ \"type\": \"Polygon\",\"coordinates\": [ [ [" + upper_left[0] + ", " + upper_left[1] + "],[" + upper_right[0] + ", " + upper_right[1] + "],[" + lower_right[0] + ", " + lower_right[1] + "], [" + lower_left[0] + ", " + lower_left[1] + "], ["+ upper_left[0] + ", " + upper_left[1] + "] ] ] }"
+            ## set in data
+            data[('spatial',)] = polygon
+
+def convert_custom_fields(key, data, errors, context):
+    """Validate and convert repeating Custom Fields."""
+    ## raise Exception('DEBUG:\n'+'\n'.join([repr(key),repr(data),repr(errors)])) # DEBUG Print args as error string
+    ## 
+    ## loop through all data and remove custom data where both key and
+    ## value are empty strings
+    keys_to_pop = []
+    for key in data:
+        ## match key = ('custom', index::int, 'custom_key')
+        if (isinstance(key,tuple) and len(key) == 3 and key[0]=='custom' and key[2]=='custom_key'):
+            index = key[1]
+            key_key = ('custom',index,'custom_key')
+            value_key = ('custom',index,'custom_value')
+            if data[key_key]=='' and data[value_key]=='':
+                keys_to_pop += [key_key,value_key]
+    for key in keys_to_pop:
+        data.pop(key)
+
+def get_validators():
+    return {
+        'ckanext_mwlr_datastore_is_year'               : is_year,
+        'ckanext_mwlr_datastore_is_date'               : is_date,
+        'ckanext_mwlr_datastore_is_number'             : is_number,
+        'ckanext_mwlr_datastore_is_elevation_range'    : is_elevation_range,
+        'ckanext_mwlr_datastore_convert_spatial'       : convert_spatial,
+        'ckanext_mwlr_datastore_convert_custom_fields' : convert_custom_fields,
+    }
--- ckanext/mwlr_datastore/plugin.py
+++ ckanext/mwlr_datastore/plugin.py
@@ -1,16 +1,92 @@
+import json
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from flask import Blueprint
 
+from ckanext.mwlr_datastore.logic import validators
+
+
+def terms_of_use():
+    return toolkit.render('terms_of_use.html')
 
 class MwlrDatastorePlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IBlueprint)
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IValidators)
+    plugins.implements(plugins.IPackageController, inherit=True)
+    plugins.implements(plugins.IFacets)
     
 
-    # IConfigurer
+    def is_fallback(self):
+        return True
+    
+    def package_types(self):
+        return []
+
+    def get_blueprint(self):
+        """Provides Flask blueprint which sets up a custom
+        `terms_of_use` URL. Uses IBlueprint interface."""
+        blueprint = Blueprint('lcrnz', self.__module__)
+        rules = [('/terms_of_use','terms_of_use',terms_of_use),]
+        for rule in rules:
+            blueprint.add_url_rule(*rule)
+        return blueprint
+
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "mwlr_datastore")
+
+
+    def get_validators(self):
+        return validators.get_validators()
+
+    def before_dataset_index(self, dataset_dict):
+        '''
+        Insert `vocab_author` into solr index with list of authors derived
+        from the dataset_dict's `author` field.
+        '''
+        def listify_author(author_value):
+            if isinstance(author_value, list):
+                return author_value
+            if author_value is None:
+                return []
+            try:
+                return json.loads(author_value)
+            except ValueError:
+                return [author_value]
+
+        author_value = listify_author(dataset_dict.get('author'))
+
+        if dataset_dict.get('author'):
+            dataset_dict['vocab_author'] = author_value
+
+        return dataset_dict
+
+    ## IFacets
+
+    def dataset_facets(self, facets_dict, package_type):
+        _update_facets(facets_dict)
+        return facets_dict
+
+    def group_facets(self, facets_dict, group_type, package_type):
+        _update_facets(facets_dict)
+        return facets_dict
+
+    def organization_facets(self, facets_dict, organization_type, package_type):
+        _update_facets(facets_dict)
+        return facets_dict
+
+
+def _update_facets(facets_dict):
+
+    facets_dict.update({
+        'vocab_author': plugins.toolkit._('Authors')
+    })
+
+
+
 
     
