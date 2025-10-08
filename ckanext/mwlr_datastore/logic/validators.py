67adaba7ccf3dfb9bd51b31f6669c68720d8814e
Alan Heays <heaysa@landcareresearch.co.nz>
2025-10-09 10:18:27 +1300

    Revert "Add validator function to convert single author string to list"
    This reverts commit aa26a8108e515af691b93209b41668e2e5bffc39.

--- ckanext/mwlr_datastore/logic/validators.py
+++ ckanext/mwlr_datastore/logic/validators.py
@@ -1,7 +1,6 @@
 import ckan.plugins.toolkit as tk
 from ckan.plugins.toolkit import Invalid
 from ckan.lib.navl.dictization_functions import Missing
-from typing import List
 import sys
 import datetime
 import math
