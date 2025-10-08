13e76957c3383094a79edda5f5d1f98193a98056
Alan Heays <heaysa@landcareresearch.co.nz>
2025-10-08 15:28:26 +1300

    Simplify validation of author field to test single-author fix
    Removed nice-to-have validation stripping whitespace from author names.

--- ckanext/mwlr_datastore/logic/validators.py
+++ ckanext/mwlr_datastore/logic/validators.py
@@ -184,25 +184,6 @@ def convert_custom_fields(key, data, errors, context):
     for key in keys_to_pop:
         data.pop(key)
 
-def encapsulate_string_in_list_if_necessary(key, data, errors, context):
-    """If a given metadata value is list do nothing, if it is a string convert to a list-encapsulate string.  Otherwise raise an error."""
-    value = data[key]
-    if isinstance(value,List):
-        for item in value:
-            if not isinstance(item,str):
-                raise Invalid(f'Input must be a string or list of strings: {repr(value)}')
-    elif isinstance(value,str):
-        data[key] = [value]
-    else:
-        raise Invalid(f'Input must be a string or list of strings: {repr(value)}')
-
-def strip_values_in_list(key, data, errors, context):
-    """Strip leading and trailing whitespace from each element of a
-    list of strings."""
-    values = data[key]
-    stripped_values = [value.strip() for value in values]
-    data[key] = stripped_values
-
 def get_validators():
     return {
         'ckanext_mwlr_datastore_is_year'               : is_year,
@@ -211,6 +192,4 @@ def get_validators():
         'ckanext_mwlr_datastore_is_elevation_range'    : is_elevation_range,
         'ckanext_mwlr_datastore_convert_spatial'       : convert_spatial,
         'ckanext_mwlr_datastore_convert_custom_fields' : convert_custom_fields,
-        'ckanext_mwlr_datastore_strip_values_in_list'  : strip_values_in_list,
-        'ckanext_mwlr_datastore_encapsulate_string_in_list_if_necessary'  : encapsulate_string_in_list_if_necessary,
     }
