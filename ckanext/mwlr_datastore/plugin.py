96ffc5156359831b40e0d210e1bd7258ca7edadd
Alan Heays <heaysa@landcareresearch.co.nz>
2025-10-22 21:56:22 +1300

    Revert "fix test: listify author in before_dataset_index"
    This reverts commit cc46d9723bf1a5d525e97c2520adf50b39197acf.
    
    Did not resolve the issue.

--- ckanext/mwlr_datastore/plugin.py
+++ ckanext/mwlr_datastore/plugin.py
@@ -61,7 +61,6 @@ class MwlrDatastorePlugin(plugins.SingletonPlugin):
 
         if dataset_dict.get('author'):
             dataset_dict['vocab_author'] = author_value
-            dataset_dict['author'] = author_value
 
         return dataset_dict
 
