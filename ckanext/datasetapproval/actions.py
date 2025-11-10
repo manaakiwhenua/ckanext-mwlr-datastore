35dd930e0e88855d3300ff1a455a3be14e976a9d
maryo1234 <olearym@landcareresearch.co.nz>
2025-11-11 12:56:07 +1300

    MWDS-222 remove draft logic, toggle visibility of pending dataset dependent on admin or editor rights

--- ckanext/datasetapproval/actions.py
+++ ckanext/datasetapproval/actions.py
@@ -28,52 +28,28 @@ def is_user_admin_of_org(org_id, user_id):
     return capacity == "admin"
 
 def publishing_check(context, data_dict):
-    log.debug("publishing_check called")
-    log.debug(f"checking context {context}")
-    user_id = (
-        toolkit.current_user.id
-        if toolkit.current_user and not toolkit.current_user.is_anonymous
-        else None
-    )
-    org_id = data_dict.get("owner_org")
-
-    is_user_editor = is_user_editor_of_org(org_id, user_id)
-    is_user_admin = is_user_admin_of_org(org_id, user_id)
-    is_sysadmin = hasattr(toolkit.current_user, "sysadmin") and toolkit.current_user.sysadmin
-
     admin_editing = context.get("admin_editing", False)
     save_as_draft = context.get("save_as_draft", False)
-    log.debug(f"checking key publishing status {data_dict.get('publishing_status')}")
-    log.debug(f"admin editing {admin_editing}")
-    log.debug(f"saving as draft {save_as_draft}")
-    log.debug(f"current reviewing status: {data_dict.get('currently_reviewing')}")
-    log.debug(f"is user editor: {is_user_editor}, is user admin: {is_user_admin}, is sysadmin: {is_sysadmin}")
+
     if data_dict.get("currently_reviewing"):
-        log.debug("removing currently_reviewing flag from data_dict")
         data_dict.pop("currently_reviewing")
         if data_dict.get("publishing_status") == "approved":
-            data_dict["state"] = "active"
+            data_dict["private"] = data_dict.get("chosen_visibility", "true") == "true"
         elif data_dict.get("publishing_status") == "rejected":
-            data_dict["state"] = "draft"
+            data_dict["private"] = "true"
     elif admin_editing:
         #if it is an admin and the dataset is being updated (not created)
         old_data_dict = toolkit.get_action("package_show")(
             context, {"id": data_dict.get("id")}
         )
-        if (is_user_admin or is_sysadmin) and old_data_dict.get("publishing_status") == "in_review":
-            data_dict["publishing_status"] = old_data_dict.get("publishing_status")
-        else:
-            data_dict['publishing_status'] = "approved"
-            data_dict["state"] = "active"
-    elif (is_user_editor or is_unowned_dataset(org_id)):
+        data_dict["publishing_status"] = old_data_dict.get("publishing_status")
+        data_dict["chosen_visibility"] = data_dict.get("private", "true")
+    else:
         if save_as_draft:
-            log.debug("saving as draft")
             #mail_package_review_request_to_admins(context, data_dict)
             data_dict['publishing_status'] = "draft"
-            data_dict["state"] = "draft"
         else:
             data_dict['publishing_status'] = "in_review"
-            data_dict["state"] = "draft"
     return data_dict
 
 
@@ -86,31 +62,28 @@ def package_show(up_func, context, data_dict):
 @toolkit.chained_action
 @logic.side_effect_free
 def package_create(up_func, context, data_dict):
-    log.debug("package_create called")
-    log.debug(f"data_dict before publishing_check: {data_dict}")
+    ## log.debug(f"data_dict before publishing_check (package_create): {data_dict}")
     publishing_check(context, data_dict)
     result = up_func(context, data_dict)
-    log.debug(f"data_dict after publishing_check: {data_dict}")
+    ## log.debug(f"data_dict after publishing_check (package_create): {data_dict}")
     return result
 
 
 @toolkit.chained_action
 @logic.side_effect_free
 def package_update(up_func, context, data_dict):
-    log.debug("package_update called")
-    log.debug(f"package_update data_dict before publishing_check: {data_dict}")
+    ## log.debug(f"data_dict before publishing_check (package_update ): {data_dict}")
     publishing_check(context, data_dict)
     result = up_func(context, data_dict)
-    log.debug(f"package_update data_dict after publishing_check: {data_dict}")
+    ## log.debug(f"data_dict after publishing_check (package_update ): {data_dict}")
     return result
 
 
 @toolkit.chained_action
 @logic.side_effect_free
 def package_patch(up_func, context, data_dict):
-    log.debug("package_patch called")
-    log.debug(f"package_patch data_dict before publishing_check: {data_dict}")
+    ## log.debug(f"data_dict before publishing_check (package_patch): {data_dict}")
     publishing_check(context, data_dict)
     result = up_func(context, data_dict)
-    log.debug(f"package_patch data_dict after publishing_check: {data_dict}")
+    ## log.debug(f"data_dict after publishing_check (package_patch): {data_dict}")
     return result
--- ckanext/datasetapproval/views/dataset.py
+++ ckanext/datasetapproval/views/dataset.py
@@ -20,6 +20,7 @@ from ckan.types import Context, Response
 import ckan.lib.navl.dictization_functions as dict_fns
 import ckan.logic as logic
 from typing import Any, Iterable, Optional, Union, cast
+from ckan.common import _
 
 log = logging.getLogger(__name__)
 
@@ -50,7 +51,7 @@ class CreateView(BaseCreateView):
         try:
             check_access(u'package_create', context)
         except NotAuthorized:
-            return base.abort(403, _(u'Unauthorized to create a package'))
+            return tk.base.abort(403, _(u'Unauthorized to create a package'))
         return context
 
     def get(
@@ -80,103 +81,6 @@ class EditView(BaseEditView):
             u'admin_editing': h.is_admin(current_user.name)
         }
         return context
-    
-    def get(self,
-            package_type: str,
-            id: str,
-            data: Optional[dict[str, Any]] = None,
-            errors: Optional[dict[str, Any]] = None,
-            error_summary: Optional[dict[str, Any]] = None
-            ) -> Union[Response, str]:
-        context = self._prepare()
-        package_type = _get_package_type(id) or package_type
-        try:
-            view_context = context.copy()
-            view_context['for_view'] = True
-            pkg_dict = tk.get_action(u'package_show')(
-                view_context, {u'id': id})
-            context[u'for_edit'] = True
-            old_data = tk.get_action(u'package_show')(context, {u'id': id})
-            # old data is from the database and data is passed from the
-            # user if there is a validation error. Use users data if there.
-            if data:
-                old_data.update(data)
-            data = old_data
-        except (NotFound, NotAuthorized):
-            return tk.base.abort(404, _(u'Dataset not found'))
-        assert data is not None
-        # are we doing a multiphase add?
-        if data.get(u'state', u'').startswith(u'draft') and data.get('publishing_status') != 'in_review':
-            tk.g.form_action = h.url_for(u'{}.new'.format(package_type))
-            tk.g.form_style = u'new'
-
-            return CreateView().get(
-                package_type,
-                data=data,
-                errors=errors,
-                error_summary=error_summary
-            )
-
-        pkg = context.get(u"package")
-        resources_json = h.dump_json(data.get(u'resources', []))
-        user = current_user.name
-        try:
-            check_access(
-                'package_update',
-                context,
-                {"id": pkg_dict.get('id')}
-            )
-        except NotAuthorized:
-            return tk.base.abort(
-                403,
-                _(u'User %r not authorized to edit %s') % (user, id)
-            )
-        # convert tags if not supplied in data
-        if data and not data.get(u'tag_string'):
-            data[u'tag_string'] = u', '.join(
-                h.dict_list_reduce(pkg_dict.get(u'tags', {}), u'name')
-            )
-        errors = errors or {}
-        form_snippet = _get_pkg_template(
-            u'package_form', package_type=package_type
-        )
-        form_vars: dict[str, Any] = {
-            u'data': data,
-            u'errors': errors,
-            u'error_summary': error_summary,
-            u'action': u'edit',
-            u'dataset_type': package_type,
-            u'form_style': u'edit'
-        }
-        errors_json = h.dump_json(errors)
-
-        # TODO: remove
-        tk.g.pkg = pkg
-        tk.g.resources_json = resources_json
-        tk.g.errors_json = errors_json
-
-        _setup_template_variables(
-            context, {u'id': id}, package_type=package_type
-        )
-
-        # we have already completed stage 1
-        form_vars[u'stage'] = [u'active']
-        if data.get(u'state', u'').startswith(u'draft'):
-            form_vars[u'stage'] = [u'active', u'complete']
-
-        edit_template = _get_pkg_template(u'edit_template', package_type)
-        return tk.base.render(
-            edit_template,
-            extra_vars={
-                u'form_vars': form_vars,
-                u'form_snippet': form_snippet,
-                u'dataset_type': package_type,
-                u'pkg_dict': pkg_dict,
-                u'pkg': pkg,
-                u'resources_json': resources_json,
-                u'errors_json': errors_json
-            }
-        )
 
 dataset.add_url_rule("/new", view_func=CreateView.as_view(str("new")))
 dataset.add_url_rule("/edit/<id>", view_func=EditView.as_view(str("edit")))
