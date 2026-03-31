83a19a2ae71c4cb3e6d53546ddbadc6b924b3f38
maryo1234 <olearym@landcareresearch.co.nz>
2026-04-01 12:34:33 +1300

    MWDS-289 grab email from config

--- ckanext/datasetapproval/plugin.py
+++ ckanext/datasetapproval/plugin.py
@@ -54,8 +54,8 @@ class DatasetapprovalPlugin(plugins.SingletonPlugin,
             'get_org_from_package_name': helpers.get_org_from_package_name,
             'vocab_label': helpers.vocab_label,
             'get_vocab_group': helpers.get_vocab_group,
-            'get_workflow_actions': helpers.get_workflow_actions,
-            'map_workflow_action_to_decision_type': helpers.map_workflow_action_to_decision_type
+            'add_reviewal_details_to_pkg': helpers.add_reviewal_details_to_pkg,
+            'retrieve_data_management_email': helpers.retrieve_data_management_email,
         }
 
     def before_search(self, search_params):
