3c7ca9ba8e267267f17224e9924957e140d19109
maryo1234 <olearym@landcareresearch.co.nz>
2026-03-31 15:43:25 +1300

    MWDS-283 put related files into a single folder

--- ckanext/datasetapproval/assets/js/feedback_form_helpers.js
+++ ckanext/datasetapproval/assets/js/feedback_form_helpers.js
@@ -1,24 +1,18 @@
-console.log("feedback_form_helpers.js loaded");
-
 document.querySelectorAll(".review-form").forEach(function(form) {
   const reviewType = form.querySelector("[name='review_type']");
   const approvalType = form.querySelector("[name='approval_type']");
-  const rejectWarning = form.querySelector(".reject-review-warning");
-  const approveWarning = form.querySelector(".approve-review-warning");
+  const reviewWarning = form.querySelector(".review-warning");
 
   function checkReviewType() {
     const reviewSelected = reviewType && reviewType.value;
     const approvalSelected = approvalType && approvalType.value;
 
     if (!reviewSelected) {
-      if (rejectWarning) rejectWarning.classList.remove("d-none");
-      if (approveWarning) approveWarning.classList.remove("d-none");
+      if (reviewWarning) reviewWarning.classList.remove("d-none");
     } else if (approvalType && !approvalSelected) {
-      if (rejectWarning) rejectWarning.classList.add("d-none");
-      if (approveWarning) approveWarning.classList.remove("d-none");
+      if (reviewWarning) reviewWarning.classList.remove("d-none");
     } else {
-      if (rejectWarning) rejectWarning.classList.add("d-none");
-      if (approveWarning) approveWarning.classList.add("d-none");
+      if (reviewWarning) reviewWarning.classList.add("d-none");
     }
   }
 
