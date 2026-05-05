9ad129ed7f4cee2e1017ce7c08953017989da174
olearym <olearym@landcareresearch.co.nz>
2026-05-05 15:37:44 +1200

    MWDS-309 clean up code

--- ckanext/datasetapproval/workflow_action_helpers.py
+++ ckanext/datasetapproval/workflow_action_helpers.py
@@ -20,13 +20,15 @@ def format_workflow_action_comment(historical_action : WorkflowHistoryEntry) -> 
     if action_type == WorkflowActionType.REJECT.value and historical_action.comment is not None:
         display_reasons = format_rejection_reasons(comment.get('rejection_reasons', '') or '')
         rejection_reason_comments : str = comment.get('rejection_reason_comments', '') or ''
-        display_comment = f"Rejection reason: '{display_reasons}'. {rejection_reason_comments.capitalize()}"
+        display_comment = f"Rejection reasons: '{display_reasons}'. {rejection_reason_comments.capitalize()}"
     elif action_type == WorkflowActionType.APPROVE.value and comment is not None:
         approval_type : str = getattr(VOCAB_ENUMS.approval_outcome, comment.get('approval_outcome', ''), '') or ''
         approval_condition_details : str = comment.get('approval_details', '') or ''
         approval_outcome_comments : str = comment.get('approval_outcome_comments', '') or ''
+        
         display_comment = [approval_type.capitalize(), approval_outcome_comments.capitalize(), approval_condition_details.capitalize()]
         display_comment = '. '.join([c for c in display_comment if c != '']) 
+
     return display_comment
 
 def map_workflow_action_to_decision_type(workflow_action : WorkflowHistoryEntry) -> str:
