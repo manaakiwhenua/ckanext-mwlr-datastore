71cb7da3230b392dcaa74d1e8a7a31c8f99463a6
Justine Waterson <watersonj@landcareresearch.co.nz>
2026-05-07 15:06:19 +1200

    MWDS-329 only query controls once

--- ckanext/datasetapproval/assets/js/field_visibility_helper.js
+++ ckanext/datasetapproval/assets/js/field_visibility_helper.js
@@ -2,31 +2,31 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form.dataset-form")
   if (!form) return;
 
-  function syncVisibility(field) {
+  function syncVisibility(controller, field, group) {
+    if (controller.type === "radio" && controller.checked == false || controller.type === "checkbox" && controller.checked == false) return; // For radio buttons and checkboxes, only proceed if the controller is checked (i.e. active)
 
-      const visibilityControllerName = field.dataset.conditionalVisibilityController;
-      const controller = form.querySelector(`[name="${visibilityControllerName}"]`);
-      if (!controller) return;
+    const controllerHasMatchingValue = String(controller.value).toLowerCase() === field.dataset.conditionalVisibilityValue.toLowerCase();
 
-      const controllerHasMatchingValue = String(controller.value).toLowerCase() === field.dataset.conditionalVisibilityValue.toLowerCase(); // For non-checkbox controller fields (e.g. a select dropdown), check if the controller field value matches the value that triggers visibility
-
-      let visible = (controller.type === "checkbox" || controller.type === "radio") ? (controller.checked && controllerHasMatchingValue) : controllerHasMatchingValue;
-
-      const group = field.closest(".form-group");
-      if (group) {
-        group.classList.toggle("d-none", !visible);
-        if (!visible) field.value = ""; // Clear the value of hidden fields to prevent stale data from being submitted
-      }
+    if (group) {
+      group.classList.toggle("d-none", !controllerHasMatchingValue);
+      if (!controllerHasMatchingValue) field.value = ""; // Clear the value of hidden fields to prevent stale data from being submitted
+    }
   }
 
   // Add change event listeners to all controls that affect field visibility
   form.querySelectorAll("[data-conditional-visibility-controller]").forEach(conditionalVisibilityField => {
+    const group = conditionalVisibilityField.closest(".form-group");
+
+    if (group) { 
+      group.classList.add("d-none"); // Hide all conditionally visible fields on page load. This means fields are hidden if the selection is blank (as with a new dataset)
+    } 
+
     const visibilityControllerName = conditionalVisibilityField.dataset.conditionalVisibilityController;
     const controller = form.querySelectorAll(`[name="${visibilityControllerName}"]`); // get all controllers with the same name (e.g. for radio button groups)
     if (controller.length > 0) {
-      syncVisibility(conditionalVisibilityField); // Sync visibility on page load 
       controller.forEach(ctrl => {
-        ctrl.addEventListener("change", () => syncVisibility(conditionalVisibilityField));
+        syncVisibility(ctrl, conditionalVisibilityField, group); // Sync visibility on page load 
+        ctrl.addEventListener("change", () => syncVisibility(ctrl, conditionalVisibilityField, group));
       });
     }
   });
